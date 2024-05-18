a206c6e890950799db7a873c866fe0761003d05b
--list <d.godfray@duocuc.cl>
2024-05-18 07:58:23 -0400

    comit jo 2

--- MiWeb/formulario.js
+++ MiWeb/formulario.js
@@ -6,40 +6,14 @@ $(document).ready(function() {
         var name = $('#nombre').val();
         var celular = $('#celular').val();
         var email = $('#email').val();
-        /*var pais = $('#pais').val();*/
-        var direccion = $('#c').val();
-        var carta = $('#carta').val();
-        var error = '';
-
-        $('.error-message').remove();
-        if(name.trim() == '') {
-            error += $('.separador #nombre').after('<div class="error-message">Por favor ingresa tu nombre.</div>');
-        }
-
-        if(celular.trim() == '') {
-            error += $('#celular').after('<div class="error-message">Por favor ingresa tu telefono.</div>');
-        }
-
-        if(email.trim() == '') {
-            error += $('#email').after('<div class="error-message">Por favor ingresa tu email.</div>');        }
-
-        if(direccion.trim() == '') {
-            error += $('#$(document).ready(function() {
-    $('#contact-form').submit(function(event) {
-
-        //event.preventDefault();
-
-        var name = $('#nombre').val();
-        var celular = $('#celular').val();
-        var email = $('#email').val();
         var direccion = $('#direccion').val();
-        var ciudad = $('#ciudad').val();
+        //var ciudad = $('#ciudad').val();
         var carta = $('#carta').val();
         var error = '';
 
         $('.error-message').remove();
         if(name.trim() == '') {
-            error += $('.separador #nombre').after('<div class="error-message">Por favor ingresa tu nombre.</div>');
+            error += $('#nombre').after('<div class="error-message">Por favor ingresa tu nombre.</div>');
         }
 
         if(celular.trim() == '') {
@@ -67,6 +41,7 @@ $(document).ready(function() {
             // Aquí puedes enviar el formulario o realizar otras acciones
             $('#mensaje-error').html('');
             alert('Formulario enviado correctamente.');
-            
-        }
-    });
+           }
+        });
+    });
+    
